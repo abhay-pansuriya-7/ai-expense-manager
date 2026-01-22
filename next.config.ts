d7aa46cd8dae0562dcdef7f5aa0b5088b912dc10
Abhay Pansuriya <100340402+abhay-pansuriya-7@users.noreply.github.com>
2026-01-22 10:33:29 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
+
+
+// ---------- OLD --------------
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
